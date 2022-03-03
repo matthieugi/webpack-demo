ca0238a50aad55f5523d0e25099f39144a1ea1ce
Matthieu Girard <matthieugirard@outlook.com>
2022-03-03 23:07:49 +0100

    init devservers

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/** @type {import('webpack').Configuration} */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: ['style-loader', 'css-loader'],
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //   ],
+  // },
+};
